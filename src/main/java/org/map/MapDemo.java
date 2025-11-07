2abf9d7a12649843e5816d28c046ccd69cea162c
Sayan Bose <42089309+sayanbose-100@users.noreply.github.com>
2025-11-07 18:02:31 +0530

    added some comments in the code

--- src/main/java/org/map/MapDemo.java
+++ src/main/java/org/map/MapDemo.java
@@ -2,33 +2,43 @@ package org.map;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class MapDemo {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 
-        /*
-         * Creating a question
-         */
+        /***********************
+         * Creating a question *
+         ***********************/
         Question q1 = new Question();
         q1.setQuestionId(1211);
         q1.setQuestion("What is Java?");
 
-        /*
-         * Cretaing an answer for the above question
-         */
+        /*********************************************
+         * Creating an answer for the above question *
+         *********************************************/
         Answer answer1 = new Answer();
         answer1.setAnswerId(121);
         answer1.setAnswer("Java is a programming Language");
         q1.setAnswer(answer1);
 
-        /*
-         * Session
-         */
+        /**************************
+         * Session Creation Start *
+         **************************/
         Session session = factory.openSession();
+        Transaction tnx = session.beginTransaction();
 
-
+        /***********************************
+         * Save the record in the database *
+         ***********************************/
+        session.persist(q1);
+        /************************
+         * Session Creation End *
+         ************************/
+        tnx.commit();
+        session.close();
         factory.close();
     }
 }
